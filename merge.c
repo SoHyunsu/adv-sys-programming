7f12f8d5c45e4c7301c7b20800245355535bd407
SoHyunsu <hs15002@kookmin.ac.kr>
2016-11-08 20:32:42 +0900

    step 2

--- merge.c
+++ merge.c
@@ -1,6 +1,6 @@
 /* 
 
-2016. 10. 21.
+2016. 11. 08.
 File merge 
 
 20113288 Hyunsu_So
@@ -15,25 +15,27 @@ File merge
 
 #define BUFFER_SIZE 2048
 #define PART_BUFFER_SIZE 200
-#define FILE_SIZE 2
+#define FILE_NUMBER 2
 
 typedef struct file_info{
-	int file_point;
+
+	int fp; // file point
+	int lp; // line point
 	int size;
-	char *buffer;
-	char *part_buffer;
-	int part_index;
+	char buf_in[2048];
+	char buf_out[200];
 	int line;
+
 }File_info;
 
 int main(int argc, char *argv[])
 {
-	FILE *file[FILE_SIZE], *fout;
+	FILE *file[FILE_NUMBER], *fout;
 	int i, j = 0;
 	char temp;
 	int end = 0;
 	int total_line = 0;
-	File_info file_info[FILE_SIZE];
+	File_info file_info[FILE_NUMBER];
 	
 	struct timeval before, after;
 	int duration;
@@ -43,7 +45,7 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "usage: %s file1 file2 fout\n", argv[0]);
 		goto leave0;
 	}
-	for (i = 0; i < FILE_SIZE; i++){
+	for (i = 0; i < FILE_NUMBER; i++){
 		if ((file[i] = fopen(argv[i + 1], "rt")) == NULL) {
 			perror(argv[i + 1]);
 			goto leave1;
@@ -55,14 +57,11 @@ int main(int argc, char *argv[])
 	}
 
 	// init file info
-	for (i = 0; i < FILE_SIZE; i++){
-		file_info[i].file_point = 0;
-		file_info[i].buffer = (char*)malloc(sizeof(char) * BUFFER_SIZE);
-		file_info[i].part_buffer = (char*)malloc(sizeof(char) * PART_BUFFER_SIZE);
-		file_info[i].part_index = 0;
-		file_info[i].line = 0;
-
-		file_info[i].size = fread(file_info[i].buffer, 1, BUFFER_SIZE, file[i]);
+	for (i = 0; i < FILE_NUMBER; i++){
+		(file_info + i)->fp = 0;
+		(file_info + i)->lp = 0;
+		(file_info + i)->line = 0;
+		(file_info + i)->size = fread((file_info + i)->buf_in, 1, BUFFER_SIZE, file[i]);
 	}
 
 	gettimeofday(&before, NULL);
@@ -70,45 +69,44 @@ int main(int argc, char *argv[])
 
 		// finish
 		end = 0;
-		for (i = 0; i < FILE_SIZE; i++){
-			if (file_info[i].size == 0){
+		for (i = 0; i < FILE_NUMBER; i++){
+			if ((file_info + i)->size == 0){
 				end++;
 			}
 		}
-		if (end == FILE_SIZE){
+		if (end == FILE_NUMBER){
 			break;
 		}
-
+		
 		// process line
-		for (i = 0; i < FILE_SIZE; i++){
+		for (i = 0; i < FILE_NUMBER; i++){
 			while(1){
 
 				// read buffer size
-				if (file_info[i].file_point == file_info[i].size){
-					file_info[i].size = fread(file_info[i].buffer, 1, BUFFER_SIZE, file[i]);
-					file_info[i].file_point = 0;
+				if ((file_info + i)->fp == (file_info + i)->size){
+					(file_info + i)->size = fread((file_info + i)->buf_in, 1, BUFFER_SIZE, file[i]);
+					(file_info + i)->fp = 0;
 				}
 
-				if(file_info[i].size == 0){
+				if ((file_info + i)->size == 0){
 					break;
 				}
-
-				file_info[i].part_buffer[file_info[i].part_index++] = file_info[i].buffer[file_info[i].file_point];
+				(file_info + i)->buf_out[(file_info + i)->lp++] = (file_info + i)->buf_in[(file_info + i)->fp];
 				
 				// end line
-				if (file_info[i].buffer[file_info[i].file_point] == '\n'){
-					for(j = 0; j < file_info[i].part_index / 2; j++){
-						temp = file_info[i].part_buffer[j];
-						file_info[i].part_buffer[j] = file_info[i].part_buffer[file_info[i].part_index - j - 1];
-						file_info[i].part_buffer[file_info[i].part_index - j - 1] = temp;
-            				}
-					fwrite(file_info[i].part_buffer, 1, file_info[i].part_index, fout);
-					file_info[i].file_point++;
-					file_info[i].line++;
-					file_info[i].part_index = 0;
+				if ((file_info + i)->buf_in[(file_info + i)->fp] == '\n'){
+					for (j = 0; j < (file_info + i)->lp / 2; j++){
+						temp = (file_info + i)->buf_out[j];
+						(file_info + i)->buf_out[j] = (file_info + i)->buf_out[(file_info + i)->lp - j - 1];
+						(file_info + i)->buf_out[(file_info + i)->lp - j - 1] = temp;
+            		}
+					fwrite((file_info + i)->buf_out, 1, (file_info + i)->lp, fout);
+					(file_info + i)->fp++;
+					(file_info + i)->line++;
+					(file_info + i)->lp = 0;
 					break;
 				}
-				file_info[i].file_point++;
+				(file_info + i)->fp++;
 			}
 		}
 	}
@@ -117,9 +115,9 @@ int main(int argc, char *argv[])
 	duration = (after.tv_sec - before.tv_sec) * 1000000 + (after.tv_usec - before.tv_usec);
 	printf("Processing time = %d.%06d sec\n", duration / 1000000, duration % 1000000);
 	
-	for (i = 0; i < FILE_SIZE; i++){
+	for (i = 0; i < FILE_NUMBER; i++){
 		printf("file_%d line : %d \n", i + 1, file_info[i].line);
-		total_line += file_info[i].line;
+		total_line += (file_info + i)->line;
 	
 	}
 	printf("total lien : %d \n", total_line);
@@ -128,7 +126,7 @@ int main(int argc, char *argv[])
 leave2:
 	fclose(fout);
 leave1:
-	for (i = 0; i < FILE_SIZE; i++){
+	for (i = 0; i < FILE_NUMBER; i++){
 		fclose(file[i]);
 	}
 leave0:
